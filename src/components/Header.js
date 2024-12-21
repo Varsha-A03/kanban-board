d2f6f04b7b7539e30c824c393c71af6216330dbb
Varsha-A03 <varshaaddanapudi@gmail.com>
2024-12-21 18:46:47 +0530

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from 'react'
-import {AppBar, ToolBar, TextField, Button} from '@mui/material';
+import {AppBar, Toolbar, TextField, Button} from '@mui/material';
 export default function Header() {
   return (
     <>
-        <AppBar position='static' sx = {{padding:'0.5rem'}}></AppBar>
+        <AppBar position='static' sx = {{padding:'0.5rem',border:'2px,solid,white'}}>
+            <Toolbar sx = {{border:'2px,solid,white',display: 'flex', justifyContent: 'space-between'}}>
+                <TextField placeholder='Search tasks...' 
+                variant='outlined' size='small' sx={{width:'72%', marginRight:'10',border:'2px,solid,white'}}></TextField>
+                <Button variant='contained' color='primary' sx={{border:'2px,solid,white',width:'25%'}}>
+                        +Add Button
+                    </Button>
+            </Toolbar>
+        </AppBar>
+        
     </>
   )
 }

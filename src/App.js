fa4b28f4a583e5067a3c7f126b07234bc6be243d
Varsha-A03 <varshaaddanapudi@gmail.com>
2024-12-21 21:15:11 +0530

    Global css Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import Header from './components/Header';
 import Board from './pages/Board';
+import './styles/Global.css';
 function App() {
   return (
-    <>
+    <div style = {{ minHeight:'100vh'}}>
       <Header />
       <Board />
-    </>
+    </div>
+    
+
   );
 }
 

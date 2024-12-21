ee502c174ebb41412829b75631422c38dc299692
Varsha-A03 <varshaaddanapudi@gmail.com>
2024-12-21 16:20:30 +0530

    App bar added

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {AppBar, ToolBar, TextField, Button} from '@mui/material';
+export default function Header() {
+  return (
+    <>
+        <AppBar position='static' sx = {{padding:'0.5rem'}}></AppBar>
+    </>
+  )
+}
